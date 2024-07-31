264b15a3daf1c76d0e9b4f6e561f1bf93178320e
Osirus Jack <methanblun@gmail.com>
2024-07-31 11:44:53 +0000

    react touter installation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
 import Todolist from "./todolist";
-
+import { DrawerDemo } from "./shadcn";
 function App() {
  
   return (
     <>
+    <DrawerDemo></DrawerDemo>
 <Todolist></Todolist>
     </>
   );
