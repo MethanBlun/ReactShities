a62acedf0e69be65c1fdbc34d32c49c4b215980b
Osirus Jack <methanblun@gmail.com>
2024-08-04 15:34:41 +0000

    destructure inside nested object

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
-import Truc from "./trucs";
+
+let someOne = {
+  name:'moi',
+  prenom:'meme',
+  parent : {
+    pere : 'yaya',
+    mere: 'mareme'
+  }
+}
+
+let { parent:{pere} } = someOne
+console.log('is',pere)
 
 
-
-
-const arr = [1,2,3,4,5,6,7]
-const  [first,second,...rest] = arr
-console.log(first,second,rest)
 
 function App() {
-  return (
-    <Truc title={"lorme"}>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit,
-        repellendus. Minus, magnam ad cumque nam eaque quam adipisci quaerat eos
-        dolorem. Labore esse adipisci debitis quaerat nobis at temporibus ipsum.
-      </p>
-      <div>i am a child div</div>
-    </Truc>
-  );
+  return 
+
+  
 }
 
 export default App;
