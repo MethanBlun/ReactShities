a1d213f3e29202c75d13348ea84ade6194e71b76
Osirus Jack <methanblun@gmail.com>
2024-08-04 11:26:09 +0000

    destructuring arr

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 import Truc from "./trucs";
 
+
+
+
+const arr = [1,2,3,4,5,6,7]
+const  [first,second,...rest] = arr
+console.log(first,second,rest)
+
 function App() {
   return (
     <Truc title={"lorme"}>
