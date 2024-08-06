5339bffb9293930c245308e2c9369d7a6a81d26e
Osirus Jack <methanblun@gmail.com>
2024-08-06 09:11:05 +0000

    destrucuture and rename

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,17 @@
+const produit = {
+  id: 1,
+  nomProduit: "Ordinateur Portable",
+  prix: 1200,
+};
 
-let someOne = {
-  name:'moi',
-  prenom:'meme',
-  parent : {
-    pere : 'yaya',
-    mere: 'mareme'
-  }
+// Utilise la destructuration pour extraire les propriétés et les renommer
+const { id: productId, nomProduit: productName, prix: productPrice } = produit;
+
+// Affiche les valeurs des variables
+console.log(productId); // doit afficher 1
+console.log(productName); // doit afficher 'Ordinateur Portable'
+console.log(productPrice); // doit afficher 1200
+
+export default function App() {
+  return <></>;
 }
-
-let { parent:{pere} } = someOne
-console.log('is',pere)
-
-
-
-function App() {
-  return 
-
-  
-}
-
-export default App;
